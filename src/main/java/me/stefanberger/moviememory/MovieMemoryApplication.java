bd1164ac2d60159c7db9d19da1ddc815b1fc4acb
Stefan Berger <sberger@magicsoftware.com>
2021-04-28 18:34:06 +0200

    dropwizard app

---
+++ src/main/java/me/stefanberger/moviememory/MovieMemoryApplication.java
@@ -0,0 +1,34 @@
+package me.stefanberger.moviememory;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class MovieMemoryApplication extends Application<MovieMemoryConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new MovieMemoryApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<MovieMemoryConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(MovieMemoryConfiguration configuration,
+                    Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
+    }
+}
